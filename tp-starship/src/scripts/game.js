d340340776fa1e3543997a5b052ffd0f6a6afd09
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-04-19 18:08:10 +0200

    impl: game.js

--- tp-starship/src/scripts/game.js
+++ tp-starship/src/scripts/game.js
@@ -1,7 +1,61 @@
+import Starship from './starship';
+import Saucer from './saucer';
+
+const alea = n => Math.floor(Math.random() * n);
+
+const START_X = 40;
 
 export default class Game {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.context = canvas.getContext("2d");
+        this.starship = new Starship(START_X, this.canvas.height / 2);
+        this.saucers = [];
+        this.score = 0;
+        this.raf = null;
+    }
 
+    addSaucer() {
+        const saucer = new Saucer(this.canvas.width, alea(this.canvas.height - Saucer.HEIGHT));
+        this.saucers.push(saucer);
+    }
+
+    moveAndDraw() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.starship.move(this.canvas);
+        this.saucers = this.saucers.filter(saucer => !saucer.move(this.canvas));
+        this.starship.draw(this.context);
+        this.saucers.forEach(saucer => saucer.draw(this.context));
+        this.raf = window.requestAnimationFrame(this.moveAndDraw.bind(this));
+    }
+
+    keyDownActionHandler(event) {
+        switch (event.key) {
+        case "ArrowUp":
+        case "Up":
+            this.starship.moveUp();
+            break;
+        case "ArrowDown":
+        case "Down":
+            this.starship.moveDown();
+            break;
+        default:
+            return;
+        }
+        event.preventDefault();
+    }
+
+    keyUpActionHandler(event) {
+        switch (event.key) {
+        case "ArrowUp":
+        case "Up":
+        case "ArrowDown":
+        case "Down":
+            this.starship.stopMoving();
+            break;
+        default:
+            return;
+        }
+        event.preventDefault();
+    }
 }
-
-
-
