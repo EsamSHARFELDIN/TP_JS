2ee7faea88ce0f8e58085deeefbd41e84b88fb76
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-04-19 18:07:15 +0200

    impl: starship.js

---
+++ tp-starship/src/scripts/starship.js
@@ -0,0 +1,49 @@
+import starshipSrc from '../assets/images/vaisseau-ballon-petit.png';
+
+import MoveState from './movestate';
+import Mobile from './mobile';
+
+const DELTA_X = 0;
+const DELTA_Y = 8;
+
+export default class Starship extends Mobile {
+    constructor(x, y) {
+        super(x, y, DELTA_X, DELTA_Y, starshipSrc);
+        this.moving = MoveState.NONE;
+    }
+
+    get up() {
+        return this.moving === MoveState.UP;
+    }
+
+    get down() {
+        return this.moving === MoveState.DOWN;
+    }
+
+    moveUp() {
+        this.deltaY = -DELTA_Y;
+        this.moving = MoveState.UP;
+    }
+
+    moveDown() {
+        this.deltaY = DELTA_Y;
+        this.moving = MoveState.DOWN;
+    }
+
+    stopMoving() {
+        this.moving = MoveState.NONE;
+    }
+
+    move(canvas) {
+        if (this.down) {
+            if (this.y + this.image.height + this.deltaY <= canvas.height) {
+                super.move(canvas);
+            }
+        }
+        else if (this.up) {
+            if (this.y + this.deltaY >= 0) {
+                super.move(canvas);
+            }
+        }
+    }
+}
