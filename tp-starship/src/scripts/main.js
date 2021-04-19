f624984c3aef49a0af2f6c83d61be6c7122033f7
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-04-19 18:10:23 +0200

    impl: game.js

--- tp-starship/src/scripts/main.js
+++ tp-starship/src/scripts/main.js
@@ -1,11 +1,17 @@
-
 // importation de la classe Game.js
 import Game from './game.js';
 
 
 // mise en place de l'action des clics sur les boutons + les gestionnaires du clavier pour contrôler le starship
 const init = () => {
+    const canvas = document.getElementById('stars');
+    const game = new Game(canvas);
 
+    window.addEventListener('keydown', game.keyDownActionHandler.bind(game));
+    window.addEventListener('keyup', game.keyUpActionHandler.bind(game));
+    document.getElementById("nouvelleSoucoupe").addEventListener('click', () => game.addSaucer());
+
+    game.moveAndDraw();
 };
 
 window.addEventListener("load",init);
