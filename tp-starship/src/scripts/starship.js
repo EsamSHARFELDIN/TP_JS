8ffb90db0f146558d96c6a3ee072adaa300bb485
zo tahina <zotahina.ratefiarivony.etu@univ-lille.fr>
2021-04-29 18:02:27 +0200

    impl: add new methods to Starship class
    * add getter and setter `isTriggerPulled`
    * add method `fire`
    * add a return value to `move`

--- tp-starship/src/scripts/starship.js
+++ tp-starship/src/scripts/starship.js
@@ -2,48 +2,67 @@ import starshipSrc from '../assets/images/vaisseau-ballon-petit.png';
 
 import MoveState from './movestate';
 import Mobile from './mobile';
+import Shoot from './shoot';
 
 const DELTA_X = 0;
 const DELTA_Y = 8;
 
 export default class Starship extends Mobile {
-    constructor(x, y) {
-        super(x, y, DELTA_X, DELTA_Y, starshipSrc);
-        this.moving = MoveState.NONE;
-    }
+  constructor(x, y) {
+    super(x, y, DELTA_X, DELTA_Y, starshipSrc);
+    this.moving = MoveState.NONE;
+    this._isTriggerPulled = false;
+ 
+  }
 
-    get up() {
-        return this.moving === MoveState.UP;
-    }
+  get isTriggerPulled() {
+    return this._isTriggerPulled;
+  }
 
-    get down() {
-        return this.moving === MoveState.DOWN;
-    }
+  set isTriggerPulled(b) {
+    this._isTriggerPulled = b;
+  }
 
-    moveUp() {
-        this.deltaY = -DELTA_Y;
-        this.moving = MoveState.UP;
-    }
+  get up() {
+    return this.moving === MoveState.UP;
+  }
 
-    moveDown() {
-        this.deltaY = DELTA_Y;
-        this.moving = MoveState.DOWN;
-    }
+  get down() {
+    return this.moving === MoveState.DOWN;
+  }
 
-    stopMoving() {
-        this.moving = MoveState.NONE;
-    }
+  moveUp() {
+    this.deltaY = -DELTA_Y;
+    this.moving = MoveState.UP;
+  }
 
-    move(canvas) {
-        if (this.down) {
-            if (this.y + this.image.height + this.deltaY <= canvas.height) {
-                super.move(canvas);
-            }
-        }
-        else if (this.up) {
-            if (this.y + this.deltaY >= 0) {
-                super.move(canvas);
-            }
-        }
+  moveDown() {
+    this.deltaY = DELTA_Y;
+    this.moving = MoveState.DOWN;
+  }
+
+  stopMoving() {
+    this.moving = MoveState.NONE;
+  }
+
+  move(canvas) {
+    if (this.down) {
+      if (this.y + this.image.height + this.deltaY <= canvas.height) {
+        super.move(canvas);
+      }
     }
+    else if (this.up) {
+      if (this.y + this.deltaY >= 0) {
+        super.move(canvas);
+      }
+    }
+    return false;
+  }
+  
+  fire() {
+    if (!this.isTriggerPulled) {
+      return new Shoot(this.x + this.width, this.y + this.height / 2);
+    }
+    return undefined;
+  }
 }
