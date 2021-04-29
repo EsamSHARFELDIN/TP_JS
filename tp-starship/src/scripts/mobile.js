45f327073eda43bc6a14376cc3f64e6dd161722a
zo tahina <zotahina.ratefiarivony.etu@univ-lille.fr>
2021-04-29 18:11:18 +0200

    impl: add new util methods to Mobile class
    * getters and setters for:
        - x
        - y
        - width
        - heigth
    * add return value to `move`

--- tp-starship/src/scripts/mobile.js
+++ tp-starship/src/scripts/mobile.js
@@ -1,24 +1,50 @@
 export default class Mobile {
     constructor(x, y, deltaX=0, deltaY=0, src) {
-        this.x= x;
-        this.y = y;
-        this.deltaX = deltaX;
-        this.deltaY = deltaY;
-        this.image = this.createImage(src);
+      this._x = x;
+      this._y = y;
+      this.deltaX = deltaX;
+      this.deltaY = deltaY;
+      this.image = this.createImage(src);
     }
-
+  
+    get x() {
+      return this._x;
+    }
+  
+    get y() {
+      return this._y;
+    }
+  
+    set x(newX) {
+      this._x = newX;
+    }
+  
+    set y(newY) {
+      this._y = newY;
+    }
+  
+    get width() {
+      return this.image.width;
+    }
+  
+    get height() {
+      return this.image.height;
+    }
+  
     createImage(src) {
-        const img = new Image();
-        img.src = src;
-        return img;
+      const img = new Image();
+      img.src = src;
+      return img;
     }
-
+  
     draw(context) {
-        context.drawImage(this.image, this.x, this.y);
+      context.drawImage(this.image, this.x, this.y);
     }
-
+  
+    /* Retourne true s'il faut détruire le mobile, false sinon */
     move(canvas) {
-        this.x += this.deltaX;
-        this.y += this.deltaY;
+      this.x += this.deltaX;
+      this.y += this.deltaY;
+      return false;
     }
-}
+  }
