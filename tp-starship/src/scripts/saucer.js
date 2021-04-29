d9d09e359035ddfd1d822ca19919e3616084c409
zo tahina <zotahina.ratefiarivony.etu@univ-lille.fr>
2021-04-29 18:07:00 +0200

    impl: add falling and being hit behaviour to Saucer class
    * new getter and setter `isHit`
    * new methods:
        - `isMissed`
        - `isDestroyed`
    * update constructor to handle the being hit behaviour

--- tp-starship/src/scripts/saucer.js
+++ tp-starship/src/scripts/saucer.js
@@ -4,23 +4,41 @@ import Mobile from './mobile';
 
 const DELTA_X = -3;
 const DELTA_Y = 0;
+const DELTA_X_FALL = 0;
+const DELTA_Y_FALL = 3;
+
 const IMAGE = new Image();
 IMAGE.src = saucerSrc;
 
 export default class Saucer extends Mobile {
-    constructor(x, y) {
-        super(x, y, DELTA_X, DELTA_Y, saucerSrc);
-    }
+  constructor(x, y) {
+    super(x, y, DELTA_X, DELTA_Y, saucerSrc);
+    this._isHit = false;
+  }
 
-    static get HEIGHT() {
-        return IMAGE.height;
-    }
+  static get HEIGHT() {
+    return IMAGE.height;
+  }
 
-    move(canvas) {
-        if (this.x + this.deltaX < 0) {
-            return true;
-        }
-        super.move(canvas);
-        return false;
-    }
+  get isHit() {
+    return this._isHit;
+  }
+
+  set isHit(b) {
+    this._isHit = b;
+  }
+
+  isMissed() {
+    return this.x < 0;
+  }
+
+  isDestroyed(canvas) {
+    return this.y > canvas.height - this.height;
+  }
+  
+  /* Make this saucer fall */
+  falls() {
+    this.deltaX = DELTA_X_FALL;
+    this.deltaY = DELTA_Y_FALL;
+  }
 }
