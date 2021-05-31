4fec5122f26b5e48643c2d40fe216490dbe7d253
Simon Legros <simon.legros.etu@univ-lille.fr>
2021-05-31 15:13:52 +0200

    fix: duplicate code and closures in setState callback
     * Removed duplicate code
     * Removed unnecessary comments
     * Added closures in setState second argument

--- projet-react-task/src/components/taskApp.jsx
+++ projet-react-task/src/components/taskApp.jsx
@@ -1,25 +1,5 @@
 import React from 'react';
 
-import '../assets/style/taskApp.css';
-
-/*
- define root component
-*/
-export default class TaskApp extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="taskApp">
-        Task App
-      </div>
-    );
-  }
-}
-import React from 'react';
-
 import ToDo from './toDo.jsx';
 import Done from './done.jsx';
 import AddTask from './addTask.jsx';
@@ -31,108 +11,98 @@ import tasksData from '../data/tasksData.js';
 const DEFAULT_PRIORITY = 1;
 
 /*
- define root component
+  define root component
 */
 export default class TaskApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {toDoList: [], doneList: []};
-    this.changePriority = this.changePriority.bind(this);
-    this.makeTaskDone = this.makeTaskDone.bind(this);
-    this.addTask = this.addTask.bind(this);
-    this.saveToDoList = this.saveToDoList.bind(this);
-    this.saveDoneList = this.saveDoneList.bind(this);
-  }
-
-  componentDidMount() {
-    /*
-    this.setState({toDoList: tasksData.map(task => 
-       ({...task, priority: DEFAULT_PRIORITY})
-    )});
-    */
-
-    this.setState({
-      toDoList: this.loadToDoList(),
-      doneList: this.loadDoneList()
-    });
-  }
-
-  saveToDoList() {
-    localStorage.setItem("toDoList", JSON.stringify(this.state.toDoList));
-  }
-
-  saveDoneList() {
-    localStorage.setItem("doneList", JSON.stringify(this.state.doneList));
-  }
-
-  loadToDoList() {
-    const toDoListJson = localStorage.getItem("toDoList");
-    if (toDoListJson === null) {
-      return [];
+    constructor(props) {
+        super(props);
+        this.state = {toDoList: [], doneList: []};
+        this.changePriority = this.changePriority.bind(this);
+        this.makeTaskDone = this.makeTaskDone.bind(this);
+        this.addTask = this.addTask.bind(this);
+        this.saveToDoList = this.saveToDoList.bind(this);
+        this.saveDoneList = this.saveDoneList.bind(this);
     }
-    else {
-      return JSON.parse(toDoListJson);
-    }
-  }
 
-  loadDoneList() {
-    const doneListJson = localStorage.getItem("doneList");
-    if(doneListJson === null) return [];
-    return JSON.parse(doneListJson);
-  }
-  
-  changePriority(idTask, newPriority) {
-    /**
-    const copyToDoList = [...this.state.toDoList];
-    const updatedTask = copyToDoList.find(task => task.id === idTask);
-    updatedTask.priority = newPriority;
-    this.setState({toDoList: copyToDoList});
-    */
-
-    this.setState(prevState => {
-      const task = prevState.toDoList.find(t => t.id === idTask);
-      task.priority = newPriority;
-      return {toDoList: prevState.toDoList};
-    }, this.saveToDoList);
-  }
-
-  makeTaskDone(idTask) {
-    this.setState(prevState => {
-      const task = prevState.toDoList.find(t => t.id === idTask);
-      return {toDoList: prevState.toDoList.filter(t => t.id !== idTask),
-              doneList: [...prevState.doneList, task]};
-    }, () => {
-      this.saveToDoList();
-      this.saveDoneList();
+    componentDidMount() {
+        this.setState({
+            toDoList: this.loadToDoList(),
+            doneList: this.loadDoneList()
         });
-  }
-  
-  addTask(desc, duration) {
-    const newId = `T${this.state.toDoList.length + this.state.doneList.length + 1}`;
-    const newTask = {
-      description: desc,
-      duration: duration,
-      priority: DEFAULT_PRIORITY,
-      id: newId
-    };
-    this.setState(prevState => ({toDoList: [...prevState.toDoList, newTask]}), this.saveToDoList);
-  }
-  
-  render() {
-    return (
-      <div className="taskApp">
-        <AddTask
-          addTask={this.addTask}
-        />
-        <ToDo
-          toDoList={this.state.toDoList}
-          changePriority={this.changePriority}
-          makeTaskDone={this.makeTaskDone}
-        />
-        <Done
-          doneList={this.state.doneList}
-        />
-      </div>
-    );
-  }
+    }
+
+    saveToDoList() {
+        localStorage.setItem("toDoList", JSON.stringify(this.state.toDoList));
+    }
+
+    saveDoneList() {
+        localStorage.setItem("doneList", JSON.stringify(this.state.doneList));
+    }
+
+    loadToDoList() {
+        const toDoListJson = localStorage.getItem("toDoList");
+        if (toDoListJson === null) {
+            return [];
+        } else {
+            return JSON.parse(toDoListJson);
+        }
+    }
+
+    loadDoneList() {
+        const doneListJson = localStorage.getItem("doneList");
+        if(doneListJson === null) {
+            return [];
+        } else {
+            return JSON.parse(doneListJson);
+        }
+    }
+
+    changePriority(idTask, newPriority) {
+        this.setState(prevState => {
+            const task = prevState.toDoList.find(t => t.id === idTask);
+            task.priority = newPriority;
+            return {toDoList: prevState.toDoList};
+        }, () => this.saveToDoList());
+    }
+
+    makeTaskDone(idTask) {
+        this.setState(prevState => {
+            const task = prevState.toDoList.find(t => t.id === idTask);
+            return {toDoList: prevState.toDoList.filter(t => t.id !== idTask),
+                    doneList: [...prevState.doneList, task]};
+        }, () => {
+            this.saveToDoList();
+            this.saveDoneList();
+        });
+    }
+
+    addTask(desc, duration) {
+        const newId = `T${this.state.toDoList.length + this.state.doneList.length + 1}`;
+        const newTask = {
+            description: desc,
+            duration: duration,
+            priority: DEFAULT_PRIORITY,
+            id: newId
+        };
+        this.setState(prevState => ({toDoList: [...prevState.toDoList, newTask]}),
+                      () => this.saveToDoList());
+    }
+
+    render() {
+        return (
+            <div className="taskApp">
+                <AddTask
+                    addTask={this.addTask}
+                />
+                <ToDo
+                    toDoList={this.state.toDoList}
+                    changePriority={this.changePriority}
+                    makeTaskDone={this.makeTaskDone}
+                />
+                <Done
+                    doneList={this.state.doneList}
+                />
+            </div>
+        );
+    }
 }
